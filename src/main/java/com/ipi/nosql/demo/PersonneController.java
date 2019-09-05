ddf7c28c7f08885cd159e73de12e07c2df671768
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2019-09-05 14:11:19 +0200

    Retourner toutes les personnes

--- src/main/java/com/ipi/nosql/demo/PersonneController.java
+++ src/main/java/com/ipi/nosql/demo/PersonneController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/personne")
 public class PersonneController {
@@ -16,5 +18,11 @@ public class PersonneController {
     @RequestMapping(value = "",method = RequestMethod.POST)
     public void insert(@RequestBody Personne p){
         repo.insert(p);
+
+    }
+
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public List<Personne> get(){
+        return repo.findAll();
     }
 }
