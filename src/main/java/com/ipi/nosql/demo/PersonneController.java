ec000f0f92f4714dece0845e23a52f5221477f22
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2019-09-05 14:17:26 +0200

    Ajout d'une methode findByName

--- src/main/java/com/ipi/nosql/demo/PersonneController.java
+++ src/main/java/com/ipi/nosql/demo/PersonneController.java
@@ -1,10 +1,7 @@
 package com.ipi.nosql.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +19,12 @@ public class PersonneController {
     }
 
     @RequestMapping(value = "",method = RequestMethod.GET)
-    public List<Personne> get(){
-        return repo.findAll();
+    public List<Personne> get(@RequestParam(value = "nom", required = false) String nom){
+        if (nom==null){
+            return repo.findAll();
+        }
+
+        return repo.findAllName(nom);
+
     }
 }
--- src/main/java/com/ipi/nosql/demo/PersonneRepository.java
+++ src/main/java/com/ipi/nosql/demo/PersonneRepository.java
@@ -1,11 +1,16 @@
 package com.ipi.nosql.demo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface PersonneRepository
         extends MongoRepository<Personne,String> {
 
+    @Query("{'nom' : ?0}")
+    public List<Personne> findAllName(String nom);
 }
